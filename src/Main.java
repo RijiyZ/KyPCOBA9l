f26f652e300ee974a04ad1b65468c1c8331b033c
llinussik <ivandikarev3000@gmail.com>
2022-11-30 16:33:55 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+    public static void calculate(int chislo) {
+        int stepen = 2;
+        System.out.printf("Число " + chislo + " в квадрате равно %.0f \n", Math.pow(chislo, stepen));
+    }
     public static void main(String[] args) {
-
+        calculate(5);
     }
 }
